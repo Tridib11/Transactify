ed181a2ef3b84a508980c16e35f9f577e41c4092
Tridib11 <tridibghosh12345@gmail.com>
2025-01-03 12:39:23 +0530

    Add routing and initial page components for authentication and dashboard

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import DashBoard from './pages/DashBoard'
+import SendMoney from './pages/SendMoney'
 
 function App() {
   return (
-    <div>
-      Hi there 
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/dashboard' element={<DashBoard/>}/>
+        <Route path='/send' element={<SendMoney/>}/>
+      </Routes>    
+    </BrowserRouter>
   )
 }
 
