a407eb9faa523e03b52bab428ac50812e65812db
Tridib11 <tridibghosh12345@gmail.com>
2025-01-03 13:26:34 +0530

    Add health check endpoint to confirm server status

--- Backend/index.js
+++ Backend/index.js
@@ -9,6 +9,12 @@ app.use(cors())
 app.use(express.json())
 app.use("/api/v1", mainRouter);
 
+app.get("/",(req,res)=>{
+  return res.status(200).json({
+    msg:"Server is Running "
+  })
+})
+
 app.listen(PORT,()=>{
   console.log(`Server started at PORT ${PORT}`)
 })
