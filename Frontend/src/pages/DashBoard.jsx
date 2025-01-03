8a7492a50309646b926ec2439cb15e1d7134924f
Tridib11 <tridibghosh12345@gmail.com>
2025-01-03 13:42:24 +0530

    Add Appbar, Balance, Users, DashBoard, and SendMoney components; update routing in App component

--- Frontend/src/pages/DashBoard.jsx
+++ Frontend/src/pages/DashBoard.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import { Appbar } from "../components/AppBar"
+import { Balance } from "../components/Balance"
+import { Users } from "../components/Users"
 
-function DashBoard() {
-  return (
-    <div>
-      
+export const DashBoard=()=>{
+  return <div>
+    <Appbar/>
+    <div className="m-8">
+      <Balance value={"10,000"}/>
+      <Users/>
     </div>
-  )
+  </div>
 }
-
-export default DashBoard
