217e2f348951a1ce103ead949b4261f2635521cb
Tridib11 <tridibghosh12345@gmail.com>
2025-01-04 12:47:27 +0530

    Add axios dependency and implement signup API call in Signup component

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -28,7 +28,14 @@ export const Signup = () => {
           setPassword(e.target.value)
         }} placeholder="your-password" label={"Password"} />
         <div className="pt-4">
-          <Button label={"Sign up"} />
+          <Button onClick={()=>{
+            axios.post("https://transactify-uwh0.onrender.com/api/v1/user/signup",{
+              username,
+              firstName,
+              lastName,
+              password
+            })
+          }} label={"Sign up"} />
         </div>
         <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
       </div>
