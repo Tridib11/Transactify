43cb7aed251b9e4fbb3227150577d209ce7db944
Tridib11 <tridibghosh12345@gmail.com>
2025-01-04 12:36:02 +0530

    Add state management for last name, username, and password in Signup component

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -7,6 +7,9 @@ import { SubHeading } from "../components/SubHeading"
 
 export const Signup = () => {
   const[firstName,setFirstName]=useState("")
+  const[lastName,setLastName]=useState("")
+  const[username,setUsername]=useState("")
+  const[password,setPassword]=useState("")
     return <div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
@@ -15,9 +18,15 @@ export const Signup = () => {
         <InputBox onChange={(e)=>{
           setFirstName(e.target.value)
         }} placeholder="John" label={"First Name"} />
-        <InputBox placeholder="Doe" label={"Last Name"} />
-        <InputBox placeholder="your-email@gmail.com" label={"Email"} />
-        <InputBox placeholder="your-password" label={"Password"} />
+        <InputBox onChange={(e)=>{
+          setLastName(e.target.value)
+        }} placeholder="Doe" label={"Last Name"} />
+        <InputBox onChange={(e)=>{
+          setUsername(e.target.value)
+        }} placeholder="your-email@gmail.com" label={"Email"} />
+        <InputBox onChange={(e)=>{
+          setPassword(e.target.value)
+        }} placeholder="your-password" label={"Password"} />
         <div className="pt-4">
           <Button label={"Sign up"} />
         </div>
