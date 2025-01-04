9dbebcd49e9b357fa89d2e6848d632c78ef8aab1
Tridib11 <tridibghosh12345@gmail.com>
2025-01-04 14:20:10 +0530

    Update index.html and App.jsx for routing improvements and asset changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes,Navigate } from 'react-router-dom'
 import {Signin} from './pages/Signin'
 import {Signup} from './pages/Signup'
 import {DashBoard} from './pages/DashBoard'
@@ -9,10 +9,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/signup' element={<Signup/>}/>
-        <Route path='/signin' element={<Signin/>}/>
-        <Route path='/dashboard' element={<DashBoard/>}/>
-        <Route path='/send' element={<SendMoney/>}/>
+        <Route path="/" element={<Navigate to="/signup" replace />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/send" element={<SendMoney />} />
       </Routes>    
     </BrowserRouter>
   )
