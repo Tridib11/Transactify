543851c2df85abbed630faa4fb20b62fcfc45ad1
Tridib11 <tridibghosh12345@gmail.com>
2025-01-04 12:33:05 +0530

    Add onChange prop to InputBox component and implement state management for first name in Signup component

--- Frontend/src/components/InputBox.jsx
+++ Frontend/src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-export function InputBox({ label, placeholder }) {
+export function InputBox({ label, placeholder,onChange }) {
   return (
     <div>
       <div className="text-sm font-medium text-left py-2">{label}</div>
--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { BottomWarning } from "../components/BottomWarning"
 import { Button } from "../components/Button"
 import { Heading } from "../components/Heading"
@@ -5,12 +6,15 @@ import { InputBox } from "../components/InputBox"
 import { SubHeading } from "../components/SubHeading"
 
 export const Signup = () => {
+  const[firstName,setFirstName]=useState("")
     return <div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
         <Heading label={"Sign up"} />
         <SubHeading label={"Enter your infromation to create an account"} />
-        <InputBox placeholder="John" label={"First Name"} />
+        <InputBox onChange={(e)=>{
+          setFirstName(e.target.value)
+        }} placeholder="John" label={"First Name"} />
         <InputBox placeholder="Doe" label={"Last Name"} />
         <InputBox placeholder="your-email@gmail.com" label={"Email"} />
         <InputBox placeholder="your-password" label={"Password"} />
